6b0e5bf7a8e3ce6bde7ea95a574943c6e170249a
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-03-09 08:20:33 -0500

    move into views.py

--- django_timer/main_app/views.py
+++ django_timer/main_app/views.py
@@ -2,3 +2,14 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+# get input from main page
+# render an html page (just use index?)
+def countdown_module(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer)
+        time.sleep(1)
+        t -= 1
+    print('Go!!')
+# return timer
